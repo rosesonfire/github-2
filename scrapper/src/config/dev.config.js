3ba4ccdf5a3e0f82f1ac3b742b7b02ad111733ab
Ishtiaque Khan <ishti.ik@gmail.com>
2018-03-02 02:21:25 +0600

    scrapper - add configuration files

---
+++ scrapper/src/config/dev.config.js
@@ -0,0 +1,9 @@
+export default {
+  scrapper: {
+    endpoint: 'https://github.com/timeline'
+  },
+  db: {
+    host: 'localhost',
+    port: 6379
+  }
+}
---
+++ scrapper/src/config/index.js
@@ -0,0 +1,12 @@
+const devFile = 'dev.conf'
+const prodFile = 'prod.conf'
+const configOptions = {
+  'production': devFile,
+  'development': prodFile
+}
+const env = process.env.NODE_ENV
+const confFile = configOptions[env] || devFile
+const config = require(`./${confFile}`)
+
+export const dbConfig = config.db
+export const scrapperConfig = config.scrapper
