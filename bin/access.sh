03de7f14097eb234c24091bf981d24c0e2a41c33
Ishtiaque Khan <ishti.ik@gmail.com>
2018-03-01 23:16:34 +0600

    Create scrapper

---
+++ bin/access.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo $1
+docker exec -it $1 /bin/sh
---
+++ scrapper/src/test/index.spec.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+
+import { expect } from './setup'
+
+describe('Dummy test', () => {
+  describe('When doing dummy test', () => {
+    it('should do nothing', () =>
+      expect(undefined).to.be.undefined
+    )
+  })
+})
