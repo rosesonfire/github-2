8fc6c464d26bcf91dd7f3f7e5326173c03f425d5
Ishtiaque Khan <ishti.ik@gmail.com>
2018-03-04 04:30:58 +0600

    scraper - fix redisODM ioc

--- scraper/src/ioc/lib/odm/redisODM.js
+++ scraper/src/ioc/lib/odm/redisODM.js
@@ -1,10 +1,14 @@
 import redisODM from './../../../main/lib/odm/redisODM'
+import { dbConfig } from '../../../config'
 
 exports = module.exports = (redisWrapper) => {
   let redisODMInstance = null
 
   try {
-    redisODMInstance = redisODM({ redis: redisWrapper })
+    const redis = redisWrapper
+    const { host, port } = dbConfig
+    
+    redisODMInstance = redisODM({ redis })({ host, port })
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error(e)
