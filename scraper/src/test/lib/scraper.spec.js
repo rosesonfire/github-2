26a869ef417a86c0690a8e026340e9ab987cc0aa
Ishtiaque Khan <ishti.ik@gmail.com>
2018-03-04 02:01:45 +0600

    scraper - remove redundant comments in scraper.spec.js

--- scraper/src/test/lib/scraper.spec.js
+++ scraper/src/test/lib/scraper.spec.js
@@ -147,57 +147,5 @@ describe('Scrapper', () => {
         ).should.eventually
           .equalTo(multipleJsonData.feed.entry.map(en => positiveReply)))
     })
-    // // eslint-disable-next-line no-undef
-    // it('should have expected properties', () =>
-    //   redisODM({ redis: redisWrapper })({ host, port }).should.include
-    //     .keys(expectedODMProperties))
-
-    // // eslint-disable-next-line no-undef
-    // describe('When creating a model object', () => {
-    //   // eslint-disable-next-line no-undef
-    //   it('should return a promise', () =>
-    //     redisODM({ redis: redisWrapper })({ host, port })
-    //       .create({ data: data, idKey }).should.be.a('promise'))
-
-    //   // eslint-disable-next-line no-undef
-    //   it('should have expected properties', () =>
-    //     redisODM({ redis: redisWrapper })({ host, port })
-    //       .create({ data: data, idKey }).should.eventually.include
-    //       .keys(expectedModelObjProperties))
-
-    //   // eslint-disable-next-line no-undef
-    //   it('should map the data properly', async () => {
-    //     const modelObj = await redisODM({ redis: redisWrapper })({ host, port })
-    //       .create({ data: data, idKey })
-    //     const modelObjData = modelObj.data
-
-    //     Object.entries(data)
-    //       .forEach(entry => modelObjData.should.have.own.property(...entry))
-
-    //     Object.entries(modelObjData)
-    //       .forEach(entry => data.should.have.own.property(...entry))
-    //   })
-    // })
   })
-
-  // // eslint-disable-next-line no-undef
-  // describe('When saving a model object', () => {
-  //   // eslint-disable-next-line no-undef
-  //   beforeEach(() => {
-  //     redisWrapperClient.hmset.once().withExactArgs(...passedData)
-  //       .resolves(positiveReply)
-  //     mocks = [ redisWrapper, redisWrapperClient.hmset ]
-  //   })
-
-  //   // eslint-disable-next-line no-undef
-  //   it('should return a promise', async () =>
-  //     (await redisODM({ redis: redisWrapper })({ host, port })
-  //       .create({ data: data, idKey })).save().should.be.a('promise'))
-
-  //   // eslint-disable-next-line no-undef
-  //   it('should be successful', async () =>
-  //     (await redisODM({ redis: redisWrapper })({ host, port })
-  //       .create({ data: data, idKey })).save().should.eventually
-  //       .equal(positiveReply))
-  // })
 })
