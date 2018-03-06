9068df5c79b7a360ef62f0f893a93d13433313e5
Ishtiaque Khan <ishti.ik@gmail.com>
2018-03-06 16:42:16 +0600

    scraper - refactor import orders

--- scraper/src/ioc/services/fetchData.js
+++ scraper/src/ioc/services/fetchData.js
@@ -1,6 +1,7 @@
+import axios from 'axios'
+
 import { createNewInstance } from './../iocHelper'
 import fetchData from './../../main/services/fetchData'
-import axios from 'axios'
 
 exports = module.exports = createNewInstance({
   instanceConstructor: fetchData,
--- scraper/src/ioc/services/getBaseUrl.js
+++ scraper/src/ioc/services/getBaseUrl.js
@@ -1,6 +1,7 @@
+import url from 'url'
+
 import { createNewInstance } from './../iocHelper'
 import getBaseUrl from './../../main/services/getBaseUrl'
-import url from 'url'
 
 exports = module.exports = createNewInstance({
   instanceConstructor: getBaseUrl,
