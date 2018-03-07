92afedcf6c53e288a0d0758f4f9b1ffc3a64e65c
Ishtiaque Khan <ishti.ik@gmail.com>
2018-03-07 17:04:17 +0600

    scraper - read db configuration from environment variable in production

--- scraper/src/config/prod.config.js
+++ scraper/src/config/prod.config.js
@@ -3,7 +3,7 @@ export default {
     endpoint: 'https://github.com/timeline'
   },
   db: {
-    host: 'localhost',
-    port: 6379
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
   }
 }
