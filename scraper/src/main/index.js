785497230fefa3f5c0c576cb548f398370e96a6a
Ishtiaque Khan <ishti.ik@gmail.com>
2018-03-08 05:38:51 +0600

    scraper - add streaming

--- scraper/src/main/index.js
+++ scraper/src/main/index.js
@@ -1,15 +1,27 @@
+import Rx from 'rxjs'
+
 import { dependencies } from './../ioc'
+import { createDefensivePromise } from './utils'
+import config from './../config'
 
 // Start scrapping
-const scrape = async () => {
+createDefensivePromise(async (resolve, reject) => {
   const scrape = await dependencies.scrape
-  const redisClient = await dependencies.redisClient
 
-  await scrape()
-  await redisClient.quit()
-}
-
-scrape().catch(e => {
+  Rx.Observable
+    .interval(config.scraper.fetchInterval)
+    // eslint-disable-next-line no-console
+    .map(_ => scrape().catch(er => console.error(er)))
+    .reduce((resultA, resultB) => Promise.all([resultA, resultB]))
+    .map(resolve)
+    .catch(reject) // this should be handled by another observable
+    .finally(resolve)
+    .subscribe()
+}).catch(e => {
   // eslint-disable-next-line no-console
   console.error(e)
+}).finally(async () => {
+  const redisClient = await dependencies.redisClient
+
+  await redisClient.quit()
 })
