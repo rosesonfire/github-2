648ea6948ece9781df05d8ae46998bbc0e797983
Ishtiaque Khan <ishti.ik@gmail.com>
2018-03-13 02:19:07 +0600

    scraperAPI - update requestBuffer ioc

--- scraperAPI/src/ioc/middlewares/requestBuffer.js
+++ scraperAPI/src/ioc/middlewares/requestBuffer.js
@@ -1,11 +1,11 @@
 import { createNewInstance } from './../iocHelper'
 import requestBuffer from './../../main/middlewares/requestBuffer'
-import { scraperApi } from './../../config'
+import config from './../../config'
 
 exports = module.exports = createNewInstance({
   instanceConstructor: requestBuffer,
   configuration: {
-    requestBufferLimit: scraperApi.requestBuffer.bufferLimit,
-    ttl: scraperApi.requestBuffer.ttl
+    requestBufferLimit: config.scraperApi.requestBuffer.bufferLimit,
+    ttl: config.scraperApi.requestBuffer.ttl
   }
 })
