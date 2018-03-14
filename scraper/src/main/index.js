0365db80f972101c17497fec9c1d6ef4bada7f96
Ishtiaque Khan <ishti.ik@gmail.com>
2018-03-14 16:45:15 +0600

    scraper - modify main/index.js

--- scraper/src/main/index.js
+++ scraper/src/main/index.js
@@ -1,3 +1,7 @@
+// TODO: this is not going to work because
+//    1. the sleep is synchronous (verify though..)
+//    2. the internal promises don't get resolved before going to
+//       next sleep cycle... :( (verify though..)
 import Rx from 'rxjs'
 
 import { dependencies } from './../ioc'
@@ -10,18 +14,19 @@ createDefensivePromise(async (resolve, reject) => {
 
   Rx.Observable
     .interval(config.scraper.fetchInterval)
+    // .take(10) // used while testing
+    .map(_ => scrape())
     // eslint-disable-next-line no-console
-    .map(_ => scrape().catch(er => console.error(er)))
-    .reduce((resultA, resultB) => Promise.all([resultA, resultB]))
-    .map(resolve)
-    .catch(reject) // this should be handled by another observable
-    .finally(resolve)
-    .subscribe()
-}).catch(e => {
-  // eslint-disable-next-line no-console
-  console.error(e)
-}).finally(async () => {
-  const redisClient = await dependencies.redisClient
-
-  await redisClient.quit()
+    .catch(e => console.error(e))
+    // .takeLast(1) // used while testing
+    // .map(resolve) // used while testing
+    .subscribe(response =>
+      response
+        // eslint-disable-next-line no-console
+        .then(response => console.log(response))
+        // eslint-disable-next-line no-console
+        .catch(er => console.error(er)))
 })
+  // eslint-disable-next-line no-console
+  .catch(e => console.error(e))
+  // .then(response => console.log(response)) // used while testing
