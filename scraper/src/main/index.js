95fcae09c2d4c679a2114f1ca6eb1a19dc192597
Ishtiaque Khan <ishti.ik@gmail.com>
2018-03-14 19:26:51 +0600

    scraper - remove unnecessary comment from main/index.js

--- scraper/src/main/index.js
+++ scraper/src/main/index.js
@@ -4,4 +4,3 @@ dependencies.stream
   .then(stream => stream())
   // eslint-disable-next-line no-console
   .catch(e => console.error(e))
-  // .then(response => console.log(response)) // used while testing
