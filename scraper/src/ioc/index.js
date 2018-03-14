3f524aeb9f7e64bcb6b826ae1dfdbabb11e00bb3
Ishtiaque Khan <ishti.ik@gmail.com>
2018-03-14 16:46:21 +0600

    remove redisClient from ioc/index.js

--- scraper/src/ioc/index.js
+++ scraper/src/ioc/index.js
@@ -3,6 +3,5 @@ import IoC from 'electrolyte'
 IoC.use(IoC.dir('dist/ioc'))
 
 export const dependencies = {
-  scrape: IoC.create('services/scrape'),
-  redisClient: IoC.create('lib/wrappers/redisClientWrapper')
+  scrape: IoC.create('services/scrape')
 }
