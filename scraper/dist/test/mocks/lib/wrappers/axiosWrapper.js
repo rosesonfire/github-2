24623e865b0ae39008342646cbe5950b31b7e330
Ishtiaque Khan <ishti.ik@gmail.com>
2018-03-14 16:47:49 +0600

    scraper - create axioWrapper mock

---
+++ scraper/dist/test/mocks/lib/wrappers/axiosWrapper.js
@@ -0,0 +1,18 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _sinon = require('sinon');
+
+var _sinon2 = _interopRequireDefault(_sinon);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+  return {
+    get: _sinon2.default.mock()
+  };
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uLy4uLy4uLy4uL3NyYy90ZXN0L21vY2tzL2xpYi93cmFwcGVycy9heGlvc1dyYXBwZXIuanMiXSwibmFtZXMiOlsiZ2V0IiwibW9jayJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUE7Ozs7OztrQkFFZTtBQUFBLFNBQU87QUFDcEJBLFNBQUssZ0JBQU1DLElBQU47QUFEZSxHQUFQO0FBQUEsQyIsImZpbGUiOiJheGlvc1dyYXBwZXIuanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgc2lub24gZnJvbSAnc2lub24nXG5cbmV4cG9ydCBkZWZhdWx0ICgpID0+ICh7XG4gIGdldDogc2lub24ubW9jaygpXG59KVxuIl19
